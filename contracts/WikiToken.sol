3c1046bb18142eb365daf6dff8b9180ec0aa43e0
PradhuumnaPancholi <pnpancholi@protonmail.com>
2021-09-27 00:36:24 -0400

    added liniter, formatter, and husky for code maintainnace and DX

--- contracts/WikiToken.sol
+++ contracts/WikiToken.sol
@@ -1,37 +1,29 @@
 // SPDX-License-Identifier: GPL-3.0//
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
-
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol';
+import '@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol';
 
 /// @author Pradhumna Pancholi
 /// @title WikiToken
 /// @notice Contract for ERC-20, governance token $WIKI for Defipedia.
-contract WikiToken is ERC20, ERC20Permit, ERC20Votes{
+contract WikiToken is ERC20, ERC20Permit, ERC20Votes {
+    constructor() ERC20('Wiki', 'WIKI') ERC20Permit('Wiki') {}
 
-    constructor() ERC20("Wiki", "WIKI") ERC20Permit("Wiki") {}
-
-    function _afterTokenTransfer(address from, address to, uint256 amount)
-        internal
-        override(ERC20, ERC20Votes)
-    {
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Votes) {
         super._afterTokenTransfer(from, to, amount);
     }
 
-    function _mint(address to, uint256 amount)
-        internal
-        override(ERC20, ERC20Votes)
-    {
+    function _mint(address to, uint256 amount) internal override(ERC20, ERC20Votes) {
         super._mint(to, amount);
     }
 
-    function _burn(address account, uint256 amount)
-        internal
-        override(ERC20, ERC20Votes)
-    {
+    function _burn(address account, uint256 amount) internal override(ERC20, ERC20Votes) {
         super._burn(account, amount);
     }
-
 }
