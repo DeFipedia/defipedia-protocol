51a0463fa7c2ee1ed40e313a5d30a16a8c199f99
PradhuumnaPancholi <pnpancholi@protonmail.com>
2021-10-03 23:33:33 -0400

    added deployment script

--- contracts/DefipediaGovernor.sol
+++ contracts/DefipediaGovernor.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0//
 pragma solidity ^0.8.0;
 
-contract DefipediaGovernor {}
+import '@openzeppelin/contracts/governance/Governor.sol';
+
+/// @title DefipediaDAO
+/// @author Pradhumna Pancholi
+contract DefipediaDAO {
+    // constructor() Governor('DefipediaDAO') {}
+}
